313a82e8186b55ce1d3687954932b57469503a5d
Clovijan Rocha <clovijan@gmail.com>
2023-04-08 17:25:01 -0300

    recuperar dados do arquivo

--- src/Automato.java
+++ src/Automato.java
@@ -1,3 +1,13 @@
+import java.io.File;
+
+import javax.lang.model.element.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.List;
 
 public class Automato {
@@ -9,6 +19,34 @@ public class Automato {
     private Estado estadoInicial;
 
     private List<Estado> estadosFinais;
+
+    public Automato populaAutomato(String pathArquivo) {
+
+        Automato automato = new Automato();
+        try {
+            File file = new File(pathArquivo);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList listaEstados = doc.getElementsByTagName("state");
+
+            for (int i = 0; i < listaEstados.getLength(); i++) {
+                Node nEstado = listaEstados.item(i);
+
+                if (nEstado.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elemento = (Element) nEstado;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return automato;
+    }
 
     public List<Estado> getEstados() {
         return this.estados;
