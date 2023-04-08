b2a22a4c48624afa2498851410ffd8f133dd6892
Clovijan Rocha <clovijan@gmail.com>
2023-04-08 18:50:56 -0300

    Criacao do metodo recupera estados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Automato automato = new Automato();
+        Estado estadosAutomato = new Estado();
+
+        automato.setEstados(estadosAutomato.recuperaEstados("C:/Users/Clovjan Rocha/Downloads/Teste.jff"));
+
+        for (Estado estado : automato.getEstados()) {
+            System.out.println("ID Estado:" + estado.getId());
+            System.out.println("Nome Estado:" + estado.getNome());
+        }
     }
 }
--- src/Estado.java
+++ src/Estado.java
@@ -1,12 +1,62 @@
+import java.io.File;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Estado {
 
     private int id;
 
     private String nome;
 
-    private boolean inicial;
+    private boolean inicial = false;
 
-    private boolean estadoFinal;
+    private boolean estadoFinal = false;
+
+    public List<Estado> recuperaEstados(String pathArquivo) {
+
+        List<Estado> estados = new ArrayList<Estado>();
+        try {
+            File file = new File(pathArquivo);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList listaEstados = doc.getElementsByTagName("state");
+
+            for (int i = 0; i < listaEstados.getLength(); i++) {
+                Node nEstado = listaEstados.item(i);
+                if (nEstado.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElemento = (Element) nEstado;
+
+                    Estado estado = new Estado();
+                    estado.setId(Integer.parseInt(eElemento.getAttribute("id")));
+                    estado.setNome(eElemento.getAttribute("name"));
+                    estado.setInicial(Boolean.parseBoolean(eElemento.getAttribute("initial")));
+                    estado.SetFinal(Boolean.parseBoolean(eElemento.getAttribute("final")));
+
+                    estados.add(estado);
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return estados;
+    }
 
     public int getId() {
         return this.id;
